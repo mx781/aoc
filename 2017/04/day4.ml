540c9543d2b4802a3b09f2f7eeb32299b4063f4f
Max Knutins <max@qqmx.eu>
2017-12-09 18:59:30 +0000

    A layer Day 4 with Ocaml

--- 2017/04/day4.ml
+++ 2017/04/day4.ml
@@ -1,24 +1,41 @@
-(* Ocaml 4.06.0 *)
 let load_file filename =
   let channel = open_in filename in
   let length = in_channel_length channel in
   let input = really_input_string channel length in
   Str.split (Str.regexp "\n") input;;
 
+let rec validate word_list =
+  match word_list with
+  | [] -> true
+  | hd::tl -> let lookup = List.filter (fun w -> w = hd) tl in
+              if (List.length lookup) > 0 then false else (validate tl);;
+
+let char_frequency word =
+  let char_list = List.map (fun s -> String.get s 0) (Str.split (Str.regexp "") word) in
+  let freqs = List.map (fun c -> (c, List.length (List.filter (fun c' -> c = c') char_list))) char_list in
+  List.sort_uniq (fun a b ->
+    let c1 = int_of_char (fst a) in
+    let c2 = int_of_char (fst b) in
+    if c1 = c2 then
+      0
+    else
+      if c1 > c2 then
+        1
+      else -1
+  ) freqs;;
+
+let rec validate2 word_list =
+  match word_list with
+  | [] -> true
+  | hd::tl -> let lookup = List.filter (fun w -> char_frequency w = char_frequency hd) tl in
+              if (List.length lookup) > 0 then false else (validate2 tl);;
+
 let valid_passphrase phrase = 
   let words = Str.split (Str.regexp " ") phrase in
-  List.map (fun x -> print_endline x) words in
-  List.fold_left (
-    fun is_valid word ->
-      if is_valid == false
-        then false
-      else
-        let all_word_instances = List.filter (fun x -> x == word) words in
-        (List.length all_word_instances) == 1
-  ) true words;;
+  validate2 words;;
 
-let passphrase_list = load_file "input" in
-let validity = List.map valid_passphrase passphrase_list in
+let passphrases = load_file "input" in
+let validity = List.map valid_passphrase passphrases in
 let valid_ones = List.filter (fun e -> e == true) validity in
 let valid_count = List.length valid_ones in
-valid_count;;
+print_endline (string_of_int valid_count);;
